56581f32863455a2ccee69d82cd07ef6b2587c69
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-07-20 09:45:23 +0200

    added filterings...

--- payment/gql_queries.py
+++ payment/gql_queries.py
@@ -14,23 +14,23 @@ class PaymentGQLType(DjangoObjectType):
         filter_fields = {
             "id": ["exact"],
             "uuid": ["exact"],
-            "status": ["exact"],
-            "expected_amount": ["exact", "lt", "lte", "gt", "gte"],
-            "received_amount": ["exact", "lt", "lte", "gt", "gte"],
-            "transfer_fee": ["exact", "lt", "lte", "gt", "gte"],
-            "officer_code": ["exact"],
-            "phone_number": ["exact", "istartswith", "icontains", "iexact"],
-            "request_date": ["exact", "lt", "lte", "gt", "gte"],
-            "received_date": ["exact", "lt", "lte", "gt", "gte"],
-            "matched_date": ["exact", "lt", "lte", "gt", "gte"],
-            "payment_date": ["exact", "lt", "lte", "gt", "gte"],
-            "date_last_sms": ["exact", "lt", "lte", "gt", "gte"],
-            "transaction_no": ["exact", "istartswith", "icontains", "iexact"],
-            "origin": ["exact", "istartswith", "icontains", "iexact"],
-            "receipt_no": ["exact", "istartswith", "icontains", "iexact"],
-            "rejected_reason": ["exact", "istartswith", "icontains", "iexact"],
-            "language_name": ["exact", "istartswith", "icontains", "iexact"],
-            "type_of_payment": ["exact", "istartswith", "icontains", "iexact"]
+            "status": ["exact", "isnull"],
+            "expected_amount": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "received_amount": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "transfer_fee": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "officer_code": ["exact", "isnull"],
+            "phone_number": ["exact", "istartswith", "icontains", "iexact", "isnull"],
+            "request_date": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "received_date": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "matched_date": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "payment_date": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "date_last_sms": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "transaction_no": ["exact", "istartswith", "icontains", "iexact", "isnull"],
+            "origin": ["exact", "istartswith", "icontains", "iexact", "isnull"],
+            "receipt_no": ["exact", "istartswith", "icontains", "iexact", "isnull"],
+            "rejected_reason": ["exact", "istartswith", "icontains", "iexact", "isnull"],
+            "language_name": ["exact", "istartswith", "icontains", "iexact", "isnull"],
+            "type_of_payment": ["exact", "istartswith", "icontains", "iexact", "isnull"]
         }
         connection_class = ExtendedConnection
 
@@ -41,12 +41,12 @@ class PaymentDetailGQLType(DjangoObjectType):
         interfaces = (graphene.relay.Node,)
         filter_fields = {
             "id": ["exact"],
-            "product_code": ["exact"],
-            "insurance_number": ["exact"],
-            "policy_stage": ["exact"],
-            "expected_amount": ["exact", "lt", "lte", "gt", "gte"],
-            "amount": ["exact", "lt", "lte", "gt", "gte"],
-            "enrollment_date": ["exact", "lt", "lte", "gt", "gte"],
+            "product_code": ["exact", "isnull"],
+            "insurance_number": ["exact", "isnull"],
+            "policy_stage": ["exact", "isnull"],
+            "expected_amount": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "amount": ["exact", "lt", "lte", "gt", "gte", "isnull"],
+            "enrollment_date": ["exact", "lt", "lte", "gt", "gte", "isnull"],
             **prefix_filterset("premium__", PremiumGQLType._meta.filter_fields),
             **prefix_filterset("payment__", PaymentGQLType._meta.filter_fields)
         }
