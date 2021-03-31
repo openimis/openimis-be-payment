dd934d0fd1b192f6d4cdbffba8eeec67504e5ba0
sniedzielski <sniedzielski@soldevelo.com>
2021-03-31 13:29:56 +0200

    OFS-257: remove blanks code line

--- payment/signals.py
+++ payment/signals.py
@@ -8,5 +8,3 @@ signal_before_payment_query = Signal(providing_args=_payment_before_query_signal
 def _read_signal_results(result_signal):
     # signal result is a representation of list of tuple (function, result)
     return [result[1] for result in result_signal if result[1] is not None]
-
-
